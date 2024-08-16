8c0d950f880abd77ee87b78ee90a49773ad49109
Swapnil Shinde <swapnilshinde@Swapnils-MacBook-Pro.local>
2024-08-16 12:19:07 +0530

    repo migration frontend

--- backend/src/config/roles.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from '@prisma/client';
-
-const allRoles = {
-  [Role.USER]: ['getUsers', 'manageUsers','createDeployment','getDeployments','getDeployment'],
-  [Role.SELLER]: ['getUsers', 'manageUsers','createDeployment','getDeployments','getDeployment']
-};
-
-export const roles = Object.keys(allRoles);
-export const roleRights = new Map(Object.entries(allRoles));
